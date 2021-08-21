c0ae8798cf03be6668905fc8caa5c4d100925d87
Gus LeTourneau <77819050+glet2024@users.noreply.github.com>
2021-08-21 02:29:00 -0500

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,12 +1,18 @@
 package SudokuSolver;
 
+/*
+* This class is responsible for creating a 2d, 9x9 integer array to
+* represent the sudoku board!
+*/
 public class Board {
 
     private Integer[][] _board = new Integer[9][9];
-
+    
+    //class constructor
     public Board() {
     }
 
+    //class copy constructor
     public Board(Board copyMe) {
         for (int j = 0; j < 9; j++) {
             for (int i = 0; i < 9; i++) {
@@ -15,10 +21,12 @@ public class Board {
         }
     }
 
+    //basic accessor (getter) method to get the board instance variable
     public Integer getBoard(int j, int i) {
         return _board[j][i];
     }
 
+    //basic mutator (setter) method to set an element of the board
     public void setBoard(Integer A, int j, int i) {
         _board[j][i] = A;
     }
